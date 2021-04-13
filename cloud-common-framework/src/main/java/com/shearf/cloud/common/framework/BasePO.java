eb16354becad4f0ad1f5eeeec001a51d92868842
xiahaihu <xiahaihu2009@gmail.com>
2021-04-13 08:56:44 +0800

    调整

--- cloud-common-framework/src/main/java/com/shearf/cloud/common/framework/BasePO.java
+++ cloud-common-framework/src/main/java/com/shearf/cloud/common/framework/BasePO.java
@@ -3,8 +3,6 @@ package com.shearf.cloud.common.framework;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableLogic;
-import lombok.Getter;
-import lombok.Setter;
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 
 import java.io.Serializable;
@@ -17,35 +15,66 @@ import java.util.Date;
  * @version 0.0.1
  * @since 2019/12/2 20:21
  */
-@Setter
-@Getter
 abstract public class BasePO implements Serializable {
 
+    private static final long serialVersionUID = 4417108425573551635L;
     /**
      * 主键
      */
     @TableId(type = IdType.AUTO)
-    private Long id;
+    protected Long id;
 
     /**
      * 逻辑删除
      */
     @TableLogic(delval = "1", value = "0")
-    private Integer del;
+    protected Integer del;
 
     /**
      * 创建时间
      */
-    private Date createTime;
+    protected Date createTime;
 
     /**
      * 更新时间
      */
-    private Date updateTime;
+    protected Date updateTime;
 
     @Override
     public String toString() {
         return new ReflectionToStringBuilder(this)
                 .toString();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getDel() {
+        return del;
+    }
+
+    public void setDel(Integer del) {
+        this.del = del;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
     }
 }
